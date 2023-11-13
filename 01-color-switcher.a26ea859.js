63c082cc3977040d93e2a673b01c6466ab14546d
CristinaSuciu26 <134690607+CristinaSuciu26@users.noreply.github.com>
2023-11-13 14:59:16 +0000

    Deploying to gh-pages from @ CristinaSuciu26/goit-js-hw-09@c222219f9983a725847accc0ca5d1975aa8323ae 🚀

---
+++ 01-color-switcher.a26ea859.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");e.addEventListener("click",(function(){t||(t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.background=t}),1e3),e.disabled=!0,a.disabled=!1)})),a.addEventListener("click",(function(){t&&(clearInterval(t),t=null),e.disabled=!1,a.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.a26ea859.js.map
